0ffa5a6f1b2e45fe05422bd45e16a4eec6b63214
chao-chen-hao <a0922866854@gmail.com>
2024-03-19 11:28:14 +0800

    add off policy ebflow

--- torch_ant_ebflow_off.py
+++ torch_ant_ebflow_off.py
@@ -1,14 +1,13 @@
-import gym
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 # import the skrl components to build the RL system
 from skrl.agents.torch.ebflow import EBFlow, EBFlow_DEFAULT_CONFIG
+from skrl.envs.loaders.torch import load_omniverse_isaacgym_env
 from skrl.envs.wrappers.torch import wrap_env
 from skrl.memories.torch import RandomMemory
-from skrl.models.torch import Model, FlowMixin
+from skrl.models.torch import FlowMixin, Model
+from skrl.resources.preprocessors.torch import RunningStandardScaler
 from skrl.trainers.torch import SequentialTrainer
 from skrl.utils import set_seed
 
@@ -30,7 +29,7 @@ class Policy(FlowMixin, Model):
         self.flows = nn.ModuleList(flows).to(self.device)
         self.prior = q0.to(self.device)
         self.alpha = alpha
-        self.unit_test(num_samples=10, scale=0.5)
+        self.unit_test(num_samples=10, scale=1)
 
     def init_Coupling(self, state_sizes, action_sizes):
         init_zeros = True
@@ -70,36 +69,36 @@ class Policy(FlowMixin, Model):
         learnable_scale_1 = MLP(scale_list, init_zeros=init_zeros, activation=activation, dropout_rate=dropout_rate_scale, layernorm=layer_norm_scale)
         learnable_scale_2 = MLP(scale_list, init_zeros=init_zeros, activation=activation, dropout_rate=dropout_rate_scale, layernorm=layer_norm_scale)
         flows += [CondScaling(learnable_scale_1, learnable_scale_2)]
-        flows += [Preprocessing(option='scaleatanh', clip=True, scale=0.5)]
+        flows += [Preprocessing(option='atanh', clip=True)]
         return flows, q0
 
-
-# load and wrap the gym environment.
-# note: the environment version may change depending on the gym version
-try:
-    env = gym.make("Pendulum-v1")
-except gym.error.DeprecatedEnv as e:
-    env_id = [spec.id for spec in gym.envs.registry.all() if spec.id.startswith("Pendulum-v")][0]
-    print("Pendulum-v1 not found. Trying {}".format(env_id))
-    env = gym.make(env_id)
+# load and wrap the Omniverse Isaac Gym environment
+env = load_omniverse_isaacgym_env(task_name="Ant", headless=True, num_envs=64)
 env = wrap_env(env)
+
 device = env.device
 
-# instantiate a memory as experience replay
-memory = RandomMemory(memory_size=20000, num_envs=env.num_envs, device=device, replacement=False)
+
+# instantiate a memory as rollout buffer (any memory can be used for this)
+memory = RandomMemory(memory_size=15625, num_envs=env.num_envs, device=device)
 
 # configure and instantiate the agent (visit its documentation to see all the options)
 # https://skrl.readthedocs.io/en/latest/api/agents/sac.html#configuration-and-hyperparameters
 cfg = EBFlow_DEFAULT_CONFIG.copy()
-cfg["discount_factor"] = 0.98
-cfg["batch_size"] = 100
-cfg["random_timesteps"] = 0
-cfg["learning_starts"] = 1 #1000
-cfg["learn_entropy"] = True
+cfg["gradient_steps"] = 1
+cfg["batch_size"] = 4096
+cfg["discount_factor"] = 0.99
+cfg["polyak"] = 0.005
+cfg["learning_rate"] = 5e-4
+cfg["random_timesteps"] = 80
+cfg["learning_starts"] = 80
+cfg["grad_norm_clip"] = 10
+cfg["state_preprocessor"] = RunningStandardScaler
+cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
 # logging to TensorBoard and write checkpoints (in timesteps)
-cfg["experiment"]["write_interval"] = 75
-cfg["experiment"]["checkpoint_interval"] = 750
-cfg["experiment"]["directory"] = "runs/torch/Pendulum"
+cfg["experiment"]["write_interval"] = 800
+cfg["experiment"]["checkpoint_interval"] = 8000
+cfg["experiment"]["directory"] = "runs/torch/Ant"
 
 # instantiate the agent's models (function approximators).
 # SAC requires 5 models, visit its documentation for more details
@@ -107,6 +106,7 @@ cfg["experiment"]["directory"] = "runs/torch/Pendulum"
 models = {}
 models["policy"] = Policy(env.observation_space, env.action_space, device, alpha=cfg['entropy_value'])
 models["target_policy"] = Policy(env.observation_space, env.action_space, device, alpha=cfg['entropy_value'])
+
 
 agent = EBFlow(models=models,
             memory=memory,
@@ -117,8 +117,8 @@ agent = EBFlow(models=models,
 
 
 # configure and instantiate the RL trainer
-cfg_trainer = {"timesteps": 15000, "headless": True}
-trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=[agent])
+cfg_trainer = {"timesteps": 160000, "headless": True}
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 
 # start training
 trainer.train()
